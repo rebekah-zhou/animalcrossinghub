8ac43e6eec99f28afea25b35135c296255209391
Rebekah Zhou <rebekahzhou@gmail.com>
2022-02-15 14:38:11 -0800

    added animated background, critters table can switch from fish sea and bugs

---
+++ achub/src/components/CritterTable.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import CritterPopup from './CritterPopup'
+
+
+const GridDiv = styled.div`
+display: grid;
+grid-template-columns: repeat(16, 60px);
+grid-auto-rows: 60px;
+border: 5px dotted grey;
+background-color: lavender;
+`
+
+const CenteredDiv = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+const StyledImg = styled.img`
+display: block;
+margin: auto;
+width: 50px;
+`
+
+// const Background = styled(CenteredDiv)`
+//   background-image: url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d19bcc31-33e4-448c-a5d4-fb8f62da91d1/dc0bkhb-5865b0f4-612e-447c-88e0-148729475f07.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2QxOWJjYzMxLTMzZTQtNDQ4Yy1hNWQ0LWZiOGY2MmRhOTFkMVwvZGMwYmtoYi01ODY1YjBmNC02MTJlLTQ0N2MtODhlMC0xNDg3Mjk0NzVmMDcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9svNSa7SddDf2bZzqgQjNmBhvWPLgbaL06rWUhAP55U');
+//   background-size: contain;
+//   overflow: auto;`
+
+function CritterTable({ selectedCritters }) {
+  const [clickedCritter, setClickedCritter] = useState({})
+
+  return (
+    <CenteredDiv>
+      <GridDiv>
+        {selectedCritters.map(critter => {
+          return <StyledImg onClick={() => setClickedCritter(critter)} key={critter.id} src={critter['icon_uri']}>
+          </StyledImg>
+        })}
+      </GridDiv>
+      <CritterPopup critter={clickedCritter}/>
+    </CenteredDiv>
+  )
+}
+
+export default CritterTable
--- achub/src/components/Critters.js
+++ achub/src/components/Critters.js
@@ -1,44 +1,62 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import CritterTable from './CritterTable'
 
-const GridDiv = styled.div`
-display: grid;
-grid-template-columns: repeat(16, 60px);
-grid-auto-rows: 60px;
-border: 5px dotted grey;
-background-color: lavender;
-`
 
-const CenteredDiv = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-`
+const StyledContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-left: 20px;
+    margin-right: 20px;
+` 
 
-const StyledImg = styled.img`
-display: block;
-margin: auto;
-width: 50px;
-border: 1px dotted black;
-`
+function Critters() {
+    
+  const [bugs, setBugs] = useState([])
+  const [sea, setSea] = useState([])
+  const [fish, setFish] = useState([])
+  const [clickValue, setClickValue] = useState('bugs')
 
-// const Background = styled(CenteredDiv)`
-//   background-image: url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d19bcc31-33e4-448c-a5d4-fb8f62da91d1/dc0bkhb-5865b0f4-612e-447c-88e0-148729475f07.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2QxOWJjYzMxLTMzZTQtNDQ4Yy1hNWQ0LWZiOGY2MmRhOTFkMVwvZGMwYmtoYi01ODY1YjBmNC02MTJlLTQ0N2MtODhlMC0xNDg3Mjk0NzVmMDcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9svNSa7SddDf2bZzqgQjNmBhvWPLgbaL06rWUhAP55U');
-//   background-size: contain;
-//   overflow: auto;`
+  let selectedCritters = bugs
 
-function Critters({ fish, bugs, seaCreatures }) {
-  const [isClicked, setIsClicked] = useState(false)
+  switch (clickValue) {
+    case 'sea':
+      selectedCritters = sea
+      break;
+    case 'fish':
+      selectedCritters = fish
+      break;
+    case 'bugs':
+      selectedCritters = bugs
+  }
 
+  useEffect(() => {
+    fetch('https://acnhapi.com/v1a/bugs')
+    .then(r => r.json())
+    .then(data => {
+      setBugs(() => data)
+    })
+  }, [])
+
+  useEffect(() => {
+    fetch('https://acnhapi.com/v1a/sea')
+    .then(r => r.json())
+    .then(data => setSea(() => data))
+  }, [])
+
+  useEffect(() => {
+    fetch('https://acnhapi.com/v1a/fish')
+    .then(r => r.json())
+    .then(data => setFish(() => data))
+  }, [])
+    
   return (
-    <CenteredDiv>
-      <GridDiv>
-        {bugs.map(bug => {
-          return <StyledImg key={bug.id} src={bug['icon_uri']}>
-          </StyledImg>
-        })}
-      </GridDiv>
-    </CenteredDiv>
+    <div>
+      <button type='button' name='bugs' onClick={(e) => setClickValue(e.target.name)}>Bugs</button>
+      <button type='button' name='fish' onClick={(e) => setClickValue(e.target.name)}>Fish</button>
+      <button type='button' name='sea' onClick={(e) => setClickValue(e.target.name)}>Sea</button>
+      <CritterTable selectedCritters={selectedCritters} />
+    </div>
   )
 }
 
