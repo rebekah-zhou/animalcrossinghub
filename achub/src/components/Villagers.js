bd09e4ea0286618de2640093a550478b08a37dc8
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-02-16 12:50:30 -0800

    style: voter on bottom

--- achub/src/components/Villagers.js
+++ achub/src/components/Villagers.js
@@ -22,24 +22,22 @@ const ContainerDiv = styled.div`
 function Villagers({ villagers, votedVillagers }) {
   const [clickedVillager, setClickedVillager] = useState(villagers.find(villager => villager.name['name-USen'] === votedVillagers[0].name))
 
-  console.log(votedVillagers)
-
-
   function handleLiClickPass(clickedVilName) {
     const foundVil = villagers.find(villager => villager.name['name-USen'] === clickedVilName)
     setClickedVillager(foundVil)
   }
 
   // Find votes related to villager on screen
-  // const highestVilVotes = [...votedVillagers].find(votedVillager => votedVillager.name === highestVotedVil.name['name-USen']).likes
   const clickedVilVotes = votedVillagers.find(votedVillager => votedVillager.name === clickedVillager.name['name-USen']).likes
 
   return (
-    <ContainerDiv>
-      <StyledDiv>{villagers.map(villager => <SideBar key={villager.name['name-USen']} name={villager.name['name-USen']} handleLiClickPass={handleLiClickPass}/>)}</StyledDiv>
-      {clickedVillager ? <Card comType='villager' dataObj={clickedVillager}/> : <h2>Loading ...</h2>}
+    <>
+      <ContainerDiv>
+        <StyledDiv>{villagers.map(villager => <SideBar key={villager.name['name-USen']} name={villager.name['name-USen']} handleLiClickPass={handleLiClickPass}/>)}</StyledDiv>
+        {clickedVillager ? <Card comType='villager' dataObj={clickedVillager}/> : <h2>Loading ...</h2>}
+      </ContainerDiv>
       {clickedVillager ? <Voter id={clickedVillager.id} name={clickedVillager.name['name-USen']} prevLikes={clickedVilVotes}/> : null}
-    </ContainerDiv>
+    </>
   )
 }
 
