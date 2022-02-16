8b0c03821c340394397af4c04140114064661c42
Rebekah Zhou <rebekahzhou@gmail.com>
2022-02-16 11:34:09 -0800

    added modal with critter pic

--- achub/src/components/CritterPopup.js
+++ achub/src/components/CritterPopup.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import ReactModal from 'react-modal'
+import Modal from 'react-modal'
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 
 // const ModalType = styled.div`
 //   display: none;
@@ -20,10 +31,43 @@ import ReactModal from 'react-modal'
 //   width: 40%;
 // `
 
-function CritterPopup({ critter }) {
+Modal.setAppElement('#root');
+
+function CritterPopup({ critter, modalIsOpen, setIsOpen }) {
+  let subtitle;
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <div></div>
-  )
+    <div>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <img src={critter.image_uri}></img>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
+    </div>
+  );
 }
 
 export default CritterPopup
