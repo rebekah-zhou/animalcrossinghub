68be256462759deb95db789d40d17e3d1dc3e7f7
Rebekah Zhou <rebekahzhou@gmail.com>
2022-02-17 13:12:47 -0800

    messed with navlinks

--- achub/src/components/NavBar.js
+++ achub/src/components/NavBar.js
@@ -1,42 +1,66 @@
 import { isLabelWithInternallyDisabledControl } from '@testing-library/user-event/dist/utils'
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 const linkStyles = {
     fontFamily: 'SweetSaturday',
-    fontSize: '40px',
+    fontSize: '50px',
     margin: "20px",
     color: 'darkGoldenRod',
-    cursor: 'url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/1385231/hand-cursor.png"), pointer'
+    cursor: 'url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/1385231/hand-cursor.png"), pointer',
+    letterSpacing: '2px',
+    textShadow: '-1px -1px 0 white, 1px -1px 0 white, -1px 1px 0 white, 1px 1px 0 white',
+    textDecoration: 'none',
 }
 
 const activeLinkStyle = {
     color: 'green'
 }
 
+const hoverStyle = {
+    fontFamily: 'SweetSaturday',
+    fontSize: '50px',
+    margin: "20px",
+    color: 'pink',
+    cursor: 'url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/1385231/hand-cursor.png"), pointer',
+    letterSpacing: '2px',
+    textShadow: '-1px -1px 0 white, 1px -1px 0 white, -1px 1px 0 white, 1px 1px 0 white',
+    textDecoration: 'none',
+}
+
 function NavBar() {
+    const [styling, setStyling] = useState(linkStyles)
+
   return (
-    <div>
-        <NavLink style={linkStyles} 
+    <div style={{'marginTop': '50px'}}>
+        <NavLink style={styling} 
             activeStyle={activeLinkStyle} 
+            onMouseEnter={() => setStyling(hoverStyle)}
+            onMouseLeave={() => setStyling(linkStyles)}
             to="/"
             exact>
             Home
         </NavLink>
-        <NavLink style={linkStyles} 
+        <NavLink style={styling} 
             activeStyle={activeLinkStyle} 
+            onMouseEnter={() => setStyling(hoverStyle)}
+            onMouseLeave={() => setStyling(linkStyles)}
             to="/villagers"
             exact>
             Villagers
         </NavLink>
-        <NavLink style={linkStyles} 
+        <NavLink style={styling} 
             activeStyle={activeLinkStyle} 
+            onMouseEnter={() => setStyling(hoverStyle)}
+            onMouseLeave={() => setStyling(linkStyles)}
             to="/fossils"
             exact>
             Fossils
         </NavLink>
-        <NavLink style={linkStyles} 
+        <NavLink style={styling} 
             activeStyle={activeLinkStyle} 
+            onMouseEnter={() => setStyling(hoverStyle)}
+            onMouseLeave={() => setStyling(linkStyles)}
             to="/critters"
             exact>
             Critters
